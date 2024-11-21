15ded4bce4760be8c8cf0c12dfce575abb512167
Jonathan135-lab <tengjt101@gmail.com>
2024-11-20 20:25:46 -0400

    Add files via upload
    My 2nd programming assignment

--- A3-P2-MessageRedaction.py
+++ A3-P2-MessageRedaction.py
@@ -7,13 +7,30 @@
 
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
+    while True:
+        phrase = input("Type a phrase (or 'quit' to exit the program): ")
+        if phrase.lower() == "quit":
+            print("Exiting program")
+            break
+        print("")
+    
+        letters_to_redact = input("Type a comma-seporated list of letters to redact: ")
+        letters_to_redact = [letter.lower() for letter in letters_to_redact.split(",")] 
 
+        redacted_phrase = ""
+        redacted_count = 0 
 
+        for char in phrase: 
+            if char.lower() in letters_to_redact:
+                redacted_phrase += "_"
+                redacted_count += 1 
+            else: 
+                redacted_phrase += char 
 
-
-
-
-
+        print(f"Number of letters readcted: {redacted_count}")
+        print(f"Redacted Phrase: {redacted_phrase}\n")
+        letters_to_redact = input("Type a comma-seporated list of letters to redact: ")
+        letters_to_redact = [letter.lower() for letter in letters_to_redact.split(",")] 
 
     # YOUR CODE ENDS HERE
 
